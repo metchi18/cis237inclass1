eb1f2f657d36591cac713c115f2e10ed05597a19
DeMetrius Bradford <metchi18@gmail.com>
2017-01-18 20:03:33 -0500

    Will create a Userinterface and employee class inside a program that will use them

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        //***************************
+        //Variable/ backing fields
+        //***************************
+        private string firstName;
+        private string lastName;
+        private decimal weeklySalary;
+
+        //**************************
+        //Properties
+        //**************************
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+        }
+        //*************************
+        //Public Methods
+        //*************************
+        public string FirstAndLastName()
+        {
+            return firstName + "  " + lastName;
+        }
+
+        public override string ToString()
+        {
+            return firstName + "  " + lastName + "  " + weeklySalary.ToString("C");
+        }
+
+        //*************************
+        //constructors
+        //*************************
+        public Employee(string FirstName, string LastName, decimal weeklySalary)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            weeklySalary = WeeklySalary;
+        }
+
+        public Employee()
+        {
+        //Do nothing
+        }
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,6 +10,63 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
+            //Make a new instance of he User Interface Class
+            UserInterface ui = new UserInterface();
+
+            //Here is a new employee being created using the default constructors
+            //we wrote in he Employee class
+            //Even though we aren't using it, it shows a single instance of one vs
+            //an array of them.
+            Employee myEmployee = new Employee();
+
+            //Let's make an array to hold a bunch of instances of Employee class
+            Employee[] employees = new Employee[10];
+
+            //Let's add some employees to our array
+            employees[0] = new Employee(" David", "Barnes", 835.00m);
+            employees[1] = new Employee("James", "Kirk", 453.00m);
+            employees[2] = new Employee("Jean-Luc", "Picard", 290.00m);
+            employees[3] = new Employee("Benjamin", "Sisco", 587.00m);
+            employees[4] = new Employee("Kathryn", "Janeway", 194.00m);
+            employees[5] = new Employee("Johnathan", "Archer", 135.00m);
+
+
+            //Get input fromt he user
+            int choice = ui.GetUserInput();
+
+            //While the choice is not 2, continue to do work
+            while (choice != 2)
+            {
+                //See if the input they sent is equal to 1.
+                if (choice == 1)
+                {
+                    string outputString = "";
+
+                    //Print out the emloyees in the array
+                    foreach (Employee employee in employees)
+                    {
+                        if (employee != null)
+                        {
+                            //Concat to the outputString
+                            outputString += employee.ToString() +
+                                Environment.NewLine;
+                        }
+
+                    }
+
+                    //use the UI class to print out the string
+                    ui.Output(outputString);
+
+                }
+
+                //re-prompt the user for input
+                choice = ui.GetUserInput();
+            }
+
+
+           
+
         }
+
     }
 }
